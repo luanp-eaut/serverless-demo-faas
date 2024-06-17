1134489e161c70860c724ce704fbfcefb5d008e5
luanp-eaut <luanp@eaut.edu.vn>
2024-06-17 14:39:03 +0700

    update

--- src/main/java/eaut/it/functions/Hello.java
+++ src/main/java/eaut/it/functions/Hello.java
@@ -10,35 +10,44 @@ public class Hello implements HttpFunction {
     @Override
     public void service(HttpRequest request, HttpResponse response)
         throws IOException {
-      response.setContentType("text/html");
+            response.setContentType("text/html");
 
-        BufferedWriter writer = response.getWriter();
-        String method = request.getMethod();
-
-        writer.write("<!DOCTYPE html>");
-        writer.write("<html lang=\"en\">");
-        writer.write("<head>");
-        writer.write("    <meta charset=\"UTF-8\">");
-        writer.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-        writer.write("    <link href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\" rel=\"stylesheet\">");
-        writer.write("    <title>Hello User</title>");
-        writer.write("</head>");
-        writer.write("<body class=\"d-flex justify-content-center align-items-center vh-100\">");
-        writer.write("    <div class=\"container\">");
-
-        if ("POST".equalsIgnoreCase(method)) {
-             String username = request.getFirstQueryParameter("username").orElse("Guest");
-            writer.write("        <div class=\"alert alert-primary\" role=\"alert\">");
-            writer.write("            <h1>Xin chào " + username + "</h1>");
+            BufferedWriter writer = response.getWriter();
+            String method = request.getMethod();
+    
+            writer.write("<!DOCTYPE html>");
+            writer.write("<html lang=\"en\">");
+            writer.write("<head>");
+            writer.write("    <meta charset=\"UTF-8\">");
+            writer.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            writer.write("    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">");
+            writer.write("    <title>Hello User</title>");
+            writer.write("</head>");
+            writer.write("<body class=\"d-flex justify-content-center align-items-center vh-100\">");
+            writer.write("    <div class=\"card\" style=\"width: 600px;\">");
+            writer.write("        <div class=\"card-body\">");
+    
+            if ("POST".equalsIgnoreCase(method)) {
+                String username = request.getFirstQueryParameter("username").orElse("Guest");
+                writer.write("            <div class=\"alert alert-primary\" role=\"alert\">");
+                writer.write("                <h1>Xin chào " + username + "</h1>");
+                writer.write("            </div>");
+            } else {
+                writer.write("            <form method=\"POST\" action=\"/serverless-demo\" class=\"form\">");
+                writer.write("                <div class=\"mb-3\">");
+                writer.write("                    <label for=\"username\" class=\"form-label\">Tên người dùng:</label>");
+                writer.write("                    <input type=\"text\" id=\"username\" name=\"username\" class=\"form-control\" placeholder=\"Tên người dùng\">");
+                writer.write("                </div>");
+                writer.write("                <button type=\"submit\" class=\"btn btn-primary\">Submit</button>");
+                writer.write("            </form>");
+            }
+    
             writer.write("        </div>");
-        } else {
-            writer.write("        <form method=\"POST\" action=\"/serverless-demo\" class=\"form-inline\">");
-            writer.write("            <div class=\"form-group mb-2\">");
-            writer.write("                <label for=\"username\" class=\"sr-only\">Tên người dùng:</label>");
-            writer.write("                <input type=\"text\" id=\"username\" name=\"username\" class=\"form-control\" placeholder=\"Tên người dùng\">");
-            writer.write("            </div>");
-            writer.write("            <button type=\"submit\" class=\"btn btn-primary mb-2\">Submit</button>");
-            writer.write("        </form>");
+            writer.write("    </div>");
+            writer.write("    <script src=\"https://code.jquery.com/jquery-3.6.0.min.js\"></script>");
+            writer.write("    <script src=\"https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js\"></script>");
+            writer.write("    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/5.3.3/js/bootstrap.min.js\"></script>");
+            writer.write("</body>");
+            writer.write("</html>");
         }
     }
-  }
