a272bc1ccff2d5ea287b998aac3f76ff11eff3c4
luanp-eaut <luanp@eaut.edu.vn>
2024-06-17 14:51:39 +0700

    update

--- src/main/java/eaut/it/functions/Hello.java
+++ src/main/java/eaut/it/functions/Hello.java
@@ -21,7 +21,7 @@ public class Hello implements HttpFunction {
             writer.write("    <meta charset=\"UTF-8\">");
             writer.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
             writer.write("    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">");
-            writer.write("    <title>Hello User</title>");
+            writer.write("    <title>Serverless Demo</title>");
             writer.write("</head>");
             writer.write("<body class=\"d-flex justify-content-center align-items-center vh-100\">");
             writer.write("    <div class=\"card\" style=\"width: 600px;\">");
@@ -29,13 +29,11 @@ public class Hello implements HttpFunction {
     
             if ("POST".equalsIgnoreCase(method)) {
                 String username = request.getFirstQueryParameter("username").orElse("Guest");
-                writer.write("            <div class=\"alert alert-primary\" role=\"alert\">");
-                writer.write("                <h1>Xin chào " + username + "</h1>");
-                writer.write("            </div>");
+                writer.write("                <h1 class=\"text-center\">Xin chào " + username + "</h1>");
             } else {
                 writer.write("            <form method=\"POST\" action=\"/serverless-demo\" class=\"form\">");
                 writer.write("                <div class=\"mb-3\">");
-                writer.write("                    <label for=\"username\" class=\"form-label\">Hãy nhập tên của bạn:</label>");
+                writer.write("                    <label for=\"username\" class=\"form-label fw-bold\">Hãy nhập tên của bạn:</label>");
                 writer.write("                    <input type=\"text\" id=\"username\" name=\"username\" class=\"form-control\" placeholder=\"Guest\">");
                 writer.write("                </div>");
                 writer.write("                <button type=\"submit\" class=\"btn btn-primary\">Submit</button>");
